808c4fb3469e5ac60ce8e60af78db36c604ce887
Jisun Lee <43569683+ljsunny@users.noreply.github.com>
2024-10-29 00:24:00 -0700

    Revert "Data Implement & Using Data REST"

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleCommentRepository.java
@@ -2,9 +2,8 @@ package com.fastcampus.fastcampusprojectboard.repository;
 
 import com.fastcampus.fastcampusprojectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource // rest api로 사용하기
+@Repository // 안붙여도 돌아감
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
 }
