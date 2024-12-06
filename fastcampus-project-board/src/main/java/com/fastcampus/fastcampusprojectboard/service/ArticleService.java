aab6f53fb5ea427bf6b84bf2aadb5bf04d95569d
ljsunny <js961223@naver.com>
2024-12-05 17:57:04 -0800

    #21 - ArticleService를 작성
    ArticleService를 작성하여 코드가 잘 작동하는 지 확인

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/service/ArticleService.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/service/ArticleService.java
@@ -1,16 +1,21 @@
 package com.fastcampus.fastcampusprojectboard.service;
 
+import com.fastcampus.fastcampusprojectboard.domain.Article;
 import com.fastcampus.fastcampusprojectboard.domain.type.SearchType;
 import com.fastcampus.fastcampusprojectboard.dto.ArticleDto;
 import com.fastcampus.fastcampusprojectboard.dto.ArticleWithCommentsDto;
 import com.fastcampus.fastcampusprojectboard.repository.ArticleRepository;
 ;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -22,21 +27,45 @@ public class ArticleService {
     public Page<ArticleDto> searchArticles(SearchType searchType,
                                            String searchKeyword,
                                            Pageable pageable) {
-        return Page.empty();
+        // 검색이 없을 때
+        if(searchKeyword == null || searchKeyword.isBlank()) {
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
+        }
+
+        return switch (searchType) {
+            case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
+        };
     }
 
     @Transactional( readOnly = true )
     public ArticleWithCommentsDto getArticle(Long articleId) {
-        return null;
+        return articleRepository.findById(articleId)
+                .map(ArticleWithCommentsDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
     public void saveArticle(ArticleDto dto) {
+        articleRepository.save(dto.toEntity());
     }
 
     public void updateArticle(ArticleDto dto) {
+        try{
+            Article article = articleRepository.getReferenceById(dto.id());
+            if(dto.title()!=null) {article.setTitle(dto.title());} // record를 사용할 시 getter setter 사용하지 않고 사용가능. 자바 14부터 반영
+            if(dto.content()!= null) {article.setContent(dto.content());}
+            article.setHashtag(dto.hashtag());
+            articleRepository.save(article);
+        }catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}" ,dto);
+        }
+
     }
 
     public void deleteArticle(long articleId) {
-
+        articleRepository.deleteById(articleId);
     }
 }
