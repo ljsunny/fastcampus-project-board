6566bf7eb0fd7d9171e3b34f92b8b24ab26acefe
ljsunny <js961223@naver.com>
2024-12-21 16:10:55 -0800

    #35-게시판, 게시글 뷰의 기능에 대응하는 자바 코드 구현
    게시판, 게시글 뷰의 기능에 대응하는 자바코드 추가.(cud)
    
    테스트를 용이하게 하기 위해 간소화한 Dto에 간소화한 of 메소드를 추가함
    
    테스트 목적 외에도,
    repository 레이어에 영속되지 않은 객체는
    메타 정보가 없는 것이 당연하므로
    새로운 데이터를 만들 때 이 부분을 입력하지 않아도 되는
    팩토리 메소드를 만들어주는 것이 의미가 있음

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/service/ArticleService.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/service/ArticleService.java
@@ -1,11 +1,13 @@
 package com.fastcampus.fastcampusprojectboard.service;
 
 import com.fastcampus.fastcampusprojectboard.domain.Article;
+import com.fastcampus.fastcampusprojectboard.domain.UserAccount;
 import com.fastcampus.fastcampusprojectboard.domain.type.SearchType;
 import com.fastcampus.fastcampusprojectboard.dto.ArticleDto;
 import com.fastcampus.fastcampusprojectboard.dto.ArticleWithCommentsDto;
 import com.fastcampus.fastcampusprojectboard.repository.ArticleRepository;
 ;
+import com.fastcampus.fastcampusprojectboard.repository.UserAccountRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +25,7 @@ import java.util.List;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final UserAccountRepository userAccountRepository;
 
     @Transactional( readOnly = true )
     public Page<ArticleDto> searchArticles(SearchType searchType,
@@ -42,20 +45,28 @@ public class ArticleService {
         };
     }
 
-    @Transactional( readOnly = true )
-    public ArticleWithCommentsDto getArticle(Long articleId) {
+    @Transactional(readOnly = true)
+    public ArticleWithCommentsDto getArticleWithComments(Long articleId) {
         return articleRepository.findById(articleId)
                 .map(ArticleWithCommentsDto::from)
                 .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
-    public void saveArticle(ArticleDto dto) {
-        articleRepository.save(dto.toEntity());
+    @Transactional( readOnly = true )
+    public ArticleDto getArticle(Long articleId) {
+        return articleRepository.findById(articleId)
+                .map(ArticleDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
-    public void updateArticle(ArticleDto dto) {
+    public void saveArticle(ArticleDto dto) {
+        UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
+        articleRepository.save(dto.toEntity(userAccount));
+    }
+
+    public void updateArticle(Long articleId, ArticleDto dto) {
         try{
-            Article article = articleRepository.getReferenceById(dto.id());
+            Article article = articleRepository.getReferenceById(articleId);
             if(dto.title()!=null) {article.setTitle(dto.title());} // record를 사용할 시 getter setter 사용하지 않고 사용가능. 자바 14부터 반영
             if(dto.content()!= null) {article.setContent(dto.content());}
             article.setHashtag(dto.hashtag());
