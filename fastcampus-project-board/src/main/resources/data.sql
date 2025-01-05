fb1252f6363b92c0420a7f458afec3668003ce0c
ljsunny <js961223@naver.com>
2025-01-05 01:12:50 -0800

    #43 - edit Password info for recognizing Password encoder
    앞머리에 `{encoding-info}`와 같이 암호화 정보를 넣어줘야
    패스워드 인코더가 읽을 수 있다.
    이에 패스워드 데이터를 알맞게 수정함

--- fastcampus-project-board/src/main/resources/data.sql
+++ fastcampus-project-board/src/main/resources/data.sql
@@ -1,10 +1,10 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, updated_at, updated_by) values
-    ('jsun', 'asdf1234', 'jsun', 'jsun@mail.com', 'I am jsun.', now(), 'jsun', now(), 'jsun')
+    ('jsun', '{noop}asdf1234', 'jsun', 'jsun@mail.com', 'I am jsun.', now(), 'jsun', now(), 'jsun')
 ;
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, updated_at, updated_by) values
-    ('jsun2', 'asdf1234', 'jsun2', 'jsun2@mail.com', 'I am jsun2.', now(), 'jsun2', now(), 'jsun2')
+    ('jsun2', '{noop}asdf1234', 'jsun2', 'jsun2@mail.com', 'I am jsun2.', now(), 'jsun2', now(), 'jsun2')
 ;
 
 -- 123 게시글
