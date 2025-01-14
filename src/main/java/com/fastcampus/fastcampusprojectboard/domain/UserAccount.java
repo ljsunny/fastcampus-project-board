86496426f55d946e724d172c694f25f9767fde47
ljsunny <js961223@naver.com>
2025-01-13 17:31:40 -0800

    #59 - Code Cleanup and Added createdBy and updatedBy to UserAccount
    Refactored and cleaned up the codebase for better readability and maintainability.
    Enhanced the UserAccount entity by adding createdBy and updatedBy fields to track the creator and updater of the account.

--- src/main/java/com/fastcampus/fastcampusprojectboard/domain/UserAccount.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/domain/UserAccount.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 import lombok.ToString;
 import java.util.Objects;
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
@@ -20,15 +20,20 @@ public class UserAccount extends AuditingFields {
     @Setter @Column(length = 100) private String nickname;
     @Setter private String memo;
     protected UserAccount() {}
-    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.email = email;
         this.nickname = nickname;
         this.memo = memo;
+        this.createdBy = createdBy;
+        this.updatedBy = createdBy;
     }
     public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
-        return new UserAccount(userId, userPassword, email, nickname, memo);
+        return new UserAccount(userId, userPassword, email, nickname, memo, null);
+    }
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
+        return new UserAccount(userId, userPassword, email, nickname, memo, createdBy);
     }
     @Override
     public boolean equals(Object o) {
